67498eac651abeb4ab20e4913a0e4b37b94593b7
Keannu Bernasol <noehbernasol0@gmail.com>
2025-11-21 23:19:11 +0800

    Transition to 3 endpoints

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,12 +3,23 @@ package api
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
+
+	goodbye "github.com/lemeow125/goapi/internal/api/goodbye"
+	hello "github.com/lemeow125/goapi/internal/api/hello"
 )
 
 func Run() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Hello world!")
+	r := mux.NewRouter()
+	
+	hello.SetupRoutes(r)
+	goodbye.SetupRoutes(r)
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, "Up!")
 	})
+	
 	fmt.Printf("Starting server...")
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", r)
 }
---
+++ internal/api/goodbye/goodbye.go
@@ -0,0 +1,14 @@
+package goodbye
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRoutes(r *mux.Router) {
+	r.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, "Goodbye!")
+	})
+}
